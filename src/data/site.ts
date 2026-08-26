15cd37d8dd09dd1305d6b609b9fc320f065a4592
esunyaya-cloud <esunyaya@gmail.com>
2026-08-26 14:47:12 +0800

    feat: add LinkedIn company page link to site footer

--- src/data/site.ts
+++ src/data/site.ts
@@ -20,6 +20,7 @@ export const COMPANY = {
   phone: '+86-579-87006734',
   email: 'Yayo@koceancleaning.com',
   whatsapp: '+8613738916022',
+  linkedin: 'https://www.linkedin.com/company/jinhua-saving-industry',
   address: 'Yiwu, Zhejiang, China',
 };
 
